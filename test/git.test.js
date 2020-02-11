9ab91f7225e86fe4c04d692b75a52dd272b2daf0
Christopher Wales <chriswales95@outlook.com>
2020-02-11 21:20:01 +0000

    add 2 new tests for Git

--- test/git.test.js
+++ test/git.test.js
@@ -1,14 +1,26 @@
 let Git = require('../src/Git');
 let git = new Git(null);
 
-it('GetRepos should fail without a token', async () =>  {
+it('GetRepos should fail without a token', async () => {
     await expect(git.getRepos(1))
         .rejects
         .toThrow('Request failed with status code 401');
 });
 
-it('GetGists should fail without a token', async () =>  {
+it('GetGists should fail without a token', async () => {
     await expect(git.getGists(1))
+        .rejects
+        .toThrow('Request failed with status code 401');
+});
+
+it('getPullRequests should fail without a token', async () => {
+    await expect(git.getPullRequests("chrisales95", "GitHero-CLI", 1))
+        .rejects
+        .toThrow('Request failed with status code 401');
+});
+
+it('getIssuesFromRepo should fail without a token', async () => {
+    await expect(git.getIssuesFromRepo("chrisales95", "GitHero-CLI", 1))
         .rejects
         .toThrow('Request failed with status code 401');
 });
