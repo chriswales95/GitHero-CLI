89487dd2d40b773598817585946d598fd3c879ac
Christopher Wales <chriswales95@outlook.com>
2020-02-11 20:33:53 +0000

    fix tests

--- test/git.test.js
+++ test/git.test.js
@@ -1,14 +1,14 @@
 let Git = require('../src/Git');
-let git = new Git();
+let git = new Git(null);
 
 it('GetRepos should fail without a token', async () =>  {
-    await expect(git.getRepos(1,null))
+    await expect(git.getRepos(1))
         .rejects
-        .toThrow('token not set');
+        .toThrow('Request failed with status code 401');
 });
 
 it('GetGists should fail without a token', async () =>  {
-    await expect(git.getGists(1,null))
+    await expect(git.getGists(1))
         .rejects
-        .toThrow('token not set');
+        .toThrow('Request failed with status code 401');
 });
